2c12c2ec41d68506fbc9f22c5dd5e0eb8f8a5e4c
Mike Flood <mike@bodhi.solar>
2023-04-24 20:10:24 -0500

    folder cleanup

---
+++ src/domain/artist/mod.rs
@@ -0,0 +1,9 @@
+mod artist;
+mod new_artist;
+mod artist_name;
+mod update_artist;
+
+pub use artist::*;
+pub use new_artist::NewArtist;
+pub use artist_name::ArtistName;
+pub use update_artist::UpdateArtist;
